b274730b93e4095a834448f1b51cf80e7f5da655
Matías Nissero <matiasnissero@gmail.com>
2023-05-18 20:30:26 -0300

    Subo lo que hizo Leandro

---
+++ mapas.js
@@ -0,0 +1,7 @@
+var mimapa = L.map('mapa').setView([-34.52302221777237, -58.70044541652767], 20);
+
+L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png', {
+    attribution: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | <a href="http://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2" target="_blank">Instituto Geográfico Nacional</a> + <a href="http://www.osm.org/copyright" target="_blank">OpenStreetMap</a>',
+    minZoom: 1,
+    maxZoom: 16
+}).addTo(mimapa);
