82abd659717e2c662d1dc20b7843850da9387086
Kruskalt <codigorap13@gmail.com>
2023-06-03 20:28:19 -0300

    dd

--- src/main/java/com/refugio/refugio/dao/UsuarioDaoImp.java
+++ src/main/java/com/refugio/refugio/dao/UsuarioDaoImp.java
@@ -10,21 +10,24 @@ import java.util.List;
 
 @Repository
 @Transactional
-public class UsuarioDaoImp implements UsuarioDao{
+public class UsuarioDaoImp implements UsuarioDao { // sss
     @PersistenceContext
     EntityManager entityManager;
+
     @Override
     public void registrar(usuario usuarios) {
-     entityManager.merge(usuarios);//merge no autocompleta el id...a la hora de mandar los datos
+        entityManager.merge(usuarios);// merge no autocompleta el id...a la hora de mandar los datos
     }
+
     @Override
     public List<usuario> getUsuario() {
         String query = "FROM usuario";
         return entityManager.createQuery(query).getResultList();
     }
+
     @Override
     public void eliminar(Long id) {
-        usuario aux =   entityManager.find(usuario.class,id);
+        usuario aux = entityManager.find(usuario.class, id);
         entityManager.remove(aux);
     }
 
