d452d661ded43c7a05968f85313bce742054db48
emiliano aguilar <1999kamerun@gmail.com>
2023-06-03 23:20:07 -0300

    lector de avisos con JSON

--- Js/app.js
+++ Js/app.js
@@ -11,16 +11,17 @@ class perfilAnimal {
 
 
 class Aviso {
-    constructor(perfilAnimal, telefono, tipoAviso) {
+    constructor(perfilAnimal, telefono, tipoAviso, img) {
         this.perfilAnimal = perfilAnimal;
         this.telefono = telefono;
         this.tipoAviso = tipoAviso;
+        this.img = img;
     }
 }
 
 class AvisoAdopcion extends Aviso {
     constructor(perfilAnimal, telefono, cuidados, tipoAviso) {
-        super(perfilAnimal, telefono, tipoAviso)
+        super(perfilAnimal, telefono, tipoAviso, img)
         this.cuidados = cuidados;
 
     }
@@ -28,7 +29,7 @@ class AvisoAdopcion extends Aviso {
 
 class avisoPerroPerdido extends Aviso {
     constructor(perfilAnimal, telefono, tipoAviso, direccion, fecha) {
-        super(perfilAnimal, telefono, tipoAviso)
+        super(perfilAnimal, telefono, tipoAviso, img)
         this.direccion = direccion;
         this.fecha = fecha;
     }
@@ -40,11 +41,60 @@ const contenerDeProcutos = document.getElementById('cuerpoHTML');
 contenerDeProcutos.classList.add("segurosMostrados")
 const adopcion = "adopcion";
 const perdido = "perdido";
+let listaSeguros = []
+
+
+
+fetch("../Js/avisos.json")
+.then(response => response.json())
+.then(data => {
+    listaSeguros = data;
+    listaSeguros.forEach((aviso) => {
+        const div = document.createElement(`div`)
+        const image = document.createElement("img")
+        image.src = aviso.img
+        //const image = document.createElement("img")
+        //  image.src = seguro.img
+        //image.classList.add("imagenSeguro")
+        div.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
+
+        <div class="row g-0">
+          <div class="col-md-4">
+          <img src="${aviso.img}" class="img-fluid rounded-start" alt="..."> 
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">  Tipo de Aviso:  ${aviso.tipoAviso}</h5>
+              <p class="card-text">Perfil del animal: ${aviso.perfilAnimal}</p>
+              <p class="card-text"><small class="text-muted">  Cuidados: ${aviso.cuidados}  </small></p>
+              <p class="card-text"><small class="text-muted">  Contacto: ${aviso.telefono}  </small></p>
+
+            </div>
+          </div>
+        </div>
+      </div>`
+
+
+
+
+
+
+        contenerDeProcutos.append(div)
+        
+
+        //       if(seguro.tipoAviso == adopcion){
+        //     }
+    })
+})
+
+
+
+
 
 const mostrarSeguros = async () => {
     console.log("jasoooooooooooooon");
 
-    fetch("./Js/avisos.json")
+    fetch("../Js/avisos.json")
         .then(response => response.json())
         .then(data => {
             listaSeguros = data;
@@ -60,7 +110,6 @@ const mostrarSeguros = async () => {
                 div.innerHTML = ` <h4 class="">  el TELEFONO $ ${seguro.telefono}</h4>
                     <hr>`
                 contenerDeProcutos.append(div)
-
 
                 //       if(seguro.tipoAviso == adopcion){
 
