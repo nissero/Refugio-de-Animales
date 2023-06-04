f4bd2220e616c313ccf2ecfc5d9ca1ac60d7d2b8
emiliano aguilar <1999kamerun@gmail.com>
2023-06-04 00:31:28 -0300

    org

---
+++ Js/appOrg.js
@@ -0,0 +1,54 @@
+class organizacion{
+    constructor(nombre, descripcion,actividades,direccion,horarios,telefono,img){
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.actividades = actividades
+        this.direccion = direccion
+        this.horarios = horarios
+        this.telefono = telefono
+        this.img = img
+    }
+}
+
+let listaOrg = []
+
+fetch("../Js/organizaciones.json")
+.then(response => response.json())
+.then(data => {
+    listaOrg = data;
+    listaOrg.forEach((org) => {
+
+
+        const div = document.createElement(`div`)
+        const image = document.createElement("img")
+        image.src = aviso.img
+        //const image = document.createElement("img")
+        //  image.src = seguro.img
+        //image.classList.add("imagenSeguro")
+        div.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
+
+        <div class="row g-0">
+          <div class="col-md-4">
+          <img src="${org.img}" class="img-fluid rounded-start" alt="..."> 
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">  Nombre ONG:  ${org.nombre}</h5>
+              <p class="card-text">Descripcion : ${org.descripcion}</p>
+              <p class="card-text">Actividades : ${org.actividades}</p>
+              <p class="card-text">Direccion : ${org.direccion}</p>
+              <p class="card-text">Horarios : ${org.horarios}</p>
+              <p class="card-text">Telefono : ${org.telefono}</p>
+
+            </div>
+          </div>
+        </div>
+      </div>`
+
+
+        contenerDeProcutos.append(div)
+        
+        //       if(seguro.tipoAviso == adopcion){
+        //     }
+    })
+})
