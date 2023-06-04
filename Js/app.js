ea4196e3c7e56e6d027115d7c0b73b86cd60efb8
Matías Nissero <matiasnissero@gmail.com>
2023-06-04 02:29:15 -0300

    hemos ganados la champions, organice app.js para que se usen funciones y guardo todo en objetos

--- Js/app.js
+++ Js/app.js
@@ -11,7 +11,8 @@ class perfilAnimal {
 
 
 class Aviso {
-    constructor(perfilAnimal, telefono, tipoAviso, img) {
+    constructor(titulo, perfilAnimal, telefono, tipoAviso, img) {
+        this.titulo = titulo;
         this.perfilAnimal = perfilAnimal;
         this.telefono = telefono;
         this.tipoAviso = tipoAviso;
@@ -20,16 +21,16 @@ class Aviso {
 }
 
 class AvisoAdopcion extends Aviso {
-    constructor(perfilAnimal, telefono, tipoAviso, img, cuidados) {
-        super(perfilAnimal, telefono, tipoAviso, img)
+    constructor(titulo, perfilAnimal, telefono, tipoAviso, img, cuidados) {
+        super(titulo, perfilAnimal, telefono, tipoAviso, img)
         this.cuidados = cuidados;
 
     }
 }
 
-class avisoPerroPerdido extends Aviso {
-    constructor(perfilAnimal, telefono, tipoAviso, img, direccion, fecha) {
-        super(perfilAnimal, telefono, tipoAviso, img)
+class avisoPerdidoEncontrado extends Aviso {
+    constructor(titulo, perfilAnimal, telefono, tipoAviso, img, direccion, fecha) {
+        super(titulo, perfilAnimal, telefono, tipoAviso, img)
         this.direccion = direccion;
         this.fecha = fecha;
     }
@@ -41,13 +42,93 @@ const contenerDeProcutos = document.getElementById('cuerpoHTML');
 contenerDeProcutos.classList.add("segurosMostrados")
 const adopcion = "adopcion";
 const perdido = "perdido";
-let listaaviso = []
+let avisos = [];
 
-function main(){
-    extraerJson();
+function exportarJson() {
+    return new Promise((resolve, reject) => {
+      let ret = [];
+      let listaaviso = [];
+      fetch("../Js/avisos.json")
+        .then(response => response.json())
+        .then(data => {
+          listaaviso = data;
+          listaaviso.forEach((aviso) => {
+            if (aviso.tipoAviso == "Adopcion") {
+              ret.push(new AvisoAdopcion(aviso.titulo, aviso.perfilAnimal, aviso.telefono, aviso.tipoAviso, aviso.img, aviso.cuidados));
+            } else if (aviso.tipoAviso == "perdidoEncontrado") {
+              ret.push(new avisoPerdidoEncontrado(aviso.titulo, aviso.perfilAnimal, aviso.telefono, aviso.tipoAviso, aviso.img, aviso.direccion, aviso.fecha));
+            }
+          });
+          resolve(ret); // Devolver el array cuando esté completo
+        })
+        .catch(error => {
+          reject(error); // Manejar el error en caso de que ocurra
+        });
+    });
 }
 
-function extraerJson() {
+exportarJson().then(nuevoArrayAvisos => {
+    avisos = nuevoArrayAvisos;
+    console.log(avisos);
+    prueba();
+});
+
+
+function prueba(){
+    avisos.forEach((aviso) => {
+                const div = document.createElement(`div`)
+                console.log(1);
+                const image = document.createElement("img")
+                image.src = aviso.img
+                //const image = document.createElement("img")
+                //  image.src = seguro.img
+                //image.classList.add("imagenSeguro")
+                if (aviso.tipoAviso == "Adopcion"){
+                    div.innerHTML = 
+                    `<div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="${aviso.img}" class="img-fluid rounded-start" alt="..."> 
+                            </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title"> ${aviso.titulo}</h5>
+                                <p class="card-text">Tipo de aviso: ${aviso.tipoAviso}</p>
+                                <p class="card-text">Perfil del animal: ${aviso.perfilAnimal}</p>
+                                <p class="card-text"><small class="text-muted">  Cuidados: ${aviso.cuidados}  </small></p>
+                                <p class="card-text"><small class="text-muted">  Contacto: ${aviso.telefono}  </small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+                contenerDeProcutos.append(div)
+            }
+            else if (aviso.tipoAviso == "perdidoEncontrado"){
+                div.innerHTML = 
+                                `<div class="card mb-3" style="max-width: 540px;">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="${aviso.img}" class="img-fluid rounded-start" alt="..."> 
+                                        </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title"> ${aviso.titulo}</h5>
+                                            <p class="card-text">Tipo de aviso: ${aviso.tipoAviso}</p>
+                                            <p class="card-text">Perfil del animal: ${aviso.perfilAnimal}</p>
+                                            <p class="card-text"><small class="text-muted">  Fecha de encuentro: ${aviso.fecha}  </small></p>
+                                            <p class="card-text"><small class="text-muted">  Contacto: ${aviso.telefono}  </small></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
+                contenerDeProcutos.append(div)
+            }
+                
+    })
+}
+
+function añadirAvisoAlDoc() {
+    let listaaviso = [];
     fetch("../Js/avisos.json")
         .then(response => response.json())
         .then(data => {
@@ -69,7 +150,8 @@ function extraerJson() {
                                         </div>
                                     <div class="col-md-8">
                                         <div class="card-body">
-                                            <h5 class="card-title">  Tipo de Aviso:  ${aviso.tipoAviso}</h5>
+                                            <h5 class="card-title"> ${aviso.titulo}</h5>
+                                            <p class="card-text">Tipo de aviso: ${aviso.tipoAviso}</p>
                                             <p class="card-text">Perfil del animal: ${aviso.perfilAnimal}</p>
                                             <p class="card-text"><small class="text-muted">  Cuidados: ${aviso.cuidados}  </small></p>
                                             <p class="card-text"><small class="text-muted">  Contacto: ${aviso.telefono}  </small></p>
