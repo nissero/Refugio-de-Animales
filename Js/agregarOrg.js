1693e8b4c87d4329c9cb89dbe8299ddc9d62b842
emiliano aguilar <1999kamerun@gmail.com>
2023-06-06 00:19:53 -0300

    commit arreglo registro

--- Js/agregarOrg.js
+++ Js/agregarOrg.js
@@ -5,41 +5,75 @@ let organizacionesNuevas = {};
 var formulario = document.getElementById("agregar-organizacion");
 
 
+var veterinariaOrg = "no";
+var refugioOrg = "no";
+var peluqueriaOrg = "no";
 
-formulario.addEventListener('click', function(event){
+formulario.addEventListener('click', function (event) {
   event.preventDefault();
 
-  var nombreORg = document.getElementById("nombre-organizacion").value;
-    var descripcionOrg = document.getElementById("descripcion-organizacion").value;
-    var veterinariaOrg = document.getElementById("veterinaria-organizacion").value;
-    var refugioOrg = document.getElementById("refugio-organizacion").value;
-    var peluqueriaOrg = document.getElementById("peluqueria-organizacion").value;
-    var direccionOrg = document.getElementById("b").value;
-    var horariosOrg = document.getElementById("horarios-organizacion").value;
-    var telefonoOrg = document.getElementById("telefono-organizacion").value;
-    var imgOrg = document.getElementById("img-organizacion").value;
 
-    var nuevaOrg = {
-      nombre: nombreORg, descripcion: descripcionOrg, veterinaria: veterinariaOrg,
-      refugioOrg: refugioOrg, peluqueria: peluqueriaOrg , direccion: direccionOrg,
-      horarios: horariosOrg, telefono: telefonoOrg, img: imgOrg
+  var checkVet = document.getElementById("veterinaria-organizacion");
+  var checkRefugio = document.getElementById("refugio-organizacion");
+  var checkPeluqueria = document.getElementById("peluqueria-organizacion");
+
+
+  if (checkVet.checked) {
+    veterinariaOrg = "si";
+  } else if (!checkVet.checked) {
+    veterinariaOrg = "no";
+
+  }
+
+  if (checkRefugio.checked) {
+    refugioOrg = "si";
+  } else if (!checkRefugio.checked) {
+    refugioOrg = "no";
+
+  }
+
+  if (checkPeluqueria.checked) {
+    peluqueriaOrg = "si";
+  } else if (!checkPeluqueria.checked) {
+    peluqueriaOrg = "no";
+
+  }
+
+
+
+
+
+  var nombreORg = document.getElementById("nombre-organizacion").value;
+  var descripcionOrg = document.getElementById("descripcion-organizacion").value;
+  //  var veterinariaOrg = document.getElementById("veterinaria-organizacion").value;
+  // var refugioOrg = document.getElementById("refugio-organizacion").value;
+  //var peluqueriaOrg = document.getElementById("peluqueria-organizacion").value;
+  var direccionOrg = document.getElementById("b").value;
+  var horariosOrg = document.getElementById("horarios-organizacion").value;
+  var telefonoOrg = document.getElementById("telefono-organizacion").value;
+  var imgOrg = document.getElementById("img-organizacion").value;
+
+  var nuevaOrg = {
+    nombre: nombreORg, descripcion: descripcionOrg, veterinaria: veterinariaOrg,
+    refugioOrg: refugioOrg, peluqueria: peluqueriaOrg, direccion: direccionOrg,
+    horarios: horariosOrg, telefono: telefonoOrg, img: imgOrg
   };
 
   // convirtiendo a JSON
- const organizacionJSON = JSON.stringify(nuevaOrg);
+  const organizacionJSON = JSON.stringify(nuevaOrg);
 
   console.log(organizacionJSON);
 
   console.log(veterinariaOrg.value)
 
-  if(nombreORg == "" || descripcionOrg == "" || descripcionOrg == "" || horariosOrg == "" || telefonoOrg == ""
-  || imgOrg == "" ){
-               Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: `El formulario esta incompleto`
-              })
-  }else{
+  if (nombreORg == "" || descripcionOrg == "" || descripcionOrg == "" || horariosOrg == "" || telefonoOrg == ""
+    || imgOrg == "") {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `El formulario esta incompleto`
+    })
+  } else {
     Swal.fire(`PERFECTO! Solicitud de registro agregada correctamente,
     Sus datos: 
     nombre: ${nombreORg}
@@ -52,7 +86,7 @@ formulario.addEventListener('click', function(event){
     telefono: ${telefonoOrg}
     Img: link de IMG guardado.
     `);
-  
+
 
   }
 
