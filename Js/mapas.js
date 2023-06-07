e4b08762e7ac95b0781399f27713dc8734a7ee53
Matías Nissero <matiasnissero@gmail.com>
2023-06-07 01:21:10 -0300

    añadi nuevos avisos

--- Js/mapas.js
+++ Js/mapas.js
@@ -122,7 +122,7 @@ function filtrado() {
 
 
 function crearMapa() {
-    map = L.map('map').setView([-34.52299128711134, -58.700488331227234], 13);
+    map = L.map('map').setView([-34.52299128711134, -58.700488331227234], 10);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
