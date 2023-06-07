e89bd6777221f59a81d0e328c381c8341e78f716
Matías Nissero <matiasnissero@gmail.com>
2023-06-06 23:56:03 -0300

    Ahora el filtro de organizacioens tambien filtra los markers en el mapa, muy bueno, bueno si estan leyendo esto me compran un cafe y un alfajor bon o bon o aguila jaja saluditos a la familia

--- Js/mapas.js
+++ Js/mapas.js
@@ -1,55 +1,182 @@
 var map;
 let markers = [];
+let markersFiltrados = [];
+const boton = document.getElementById("actualizar-organizacion");
+const verTodosLosMarkers = document.getElementById("ver-todo");
+var actualizarOrganizaciones = document.getElementById("actualizar-organizacion");
+boton.addEventListener('click', mostrarOrganizacionFiltrada);
+var filtroVeterinariaIsChecked = true;
+var filtropeluqueriaIsChecked = true;
+var filtrorefugioIsChecked = true;
 
 crearMapa();
 exportarJson().then(arrayDeMarkers => {
     markers = arrayDeMarkers;
-    añadirMarkers();
+    checkVet();
+    checkRefugio();
+    checkpeluqueria();
+    filtrado();
+    añadirMarkers(markersFiltrados);
 });
 
-function crearMapa(){
-    map = L.map('map').setView([-34.52299128711134, -58.700488331227234], 13);
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(map);
+function checkRefugio() {
+    var filtroRefugio = document.querySelector("input[name=refugio]");
+    filtroRefugio.addEventListener('change', function () {
+        if (this.checked) {
+            // filtroPerdidoIsChecked = true;
+            console.log("check refugio");
+            filtrorefugioIsChecked = true;
+            return filtrorefugioIsChecked;
+        }
+        else {
+          filtrorefugioIsChecked = false;
+            return filtrorefugioIsChecked;
+        }
+    });
 }
 
-function añadirMarkers(){
-    markers.forEach((marker) => {
-        map.addLayer(marker);
+
+function checkpeluqueria() {
+  var filtroPeluqueria = document.querySelector("input[name=peluqueria]");
+  filtroPeluqueria.addEventListener('change', function () {
+      if (this.checked) {
+          // filtroPerdidoIsChecked = true;
+          console.log("check peluqueria");
+          filtropeluqueriaIsChecked = true;
+          return filtropeluqueriaIsChecked;
+      }
+      else {
+        filtropeluqueriaIsChecked = false;
+          return filtropeluqueriaIsChecked;
+      }
+  });
+}
+
+function mostrarOrganizacionFiltrada() {
+    removerMarkers(markersFiltrados);
+    checkVet();
+    checkRefugio();
+    checkpeluqueria();
+    filtrado();
+    if (filtroVeterinariaIsChecked || filtropeluqueriaIsChecked || filtrorefugioIsChecked){
+        añadirMarkers(markersFiltrados);
+    }
+    
+}
+
+function mostrarTodasLasOrg(){
+    removerMarkers(markersFiltrados);
+    añadirMarkers(markers);
+}
+
+function filtrado() {
+    if (filtroVeterinariaIsChecked && filtropeluqueriaIsChecked
+        && filtrorefugioIsChecked) {
+        markersFiltrados = markers.filter(marker =>
+            marker.options.veterinaria || marker.options.peluqueria || marker.options.refugio)
+    }
+    //VER vete si y pelu si
+    else if (filtroVeterinariaIsChecked && filtropeluqueriaIsChecked
+        && !filtrorefugioIsChecked) {
+        markersFiltrados = markers.filter(marker =>
+            (marker.options.veterinaria || marker.options.peluqueria));
+    }
+    // VER vet si, pelu no , refu si
+    else if (filtroVeterinariaIsChecked && !filtropeluqueriaIsChecked
+        && filtrorefugioIsChecked) {
+
+        markersFiltrados = markers.filter(marker =>
+            marker.options.veterinaria || marker.options.refugio);
+    }
+    // VER pelu si, pelu NO, refu NO
+    else if (filtroVeterinariaIsChecked && !filtropeluqueriaIsChecked
+        && !filtrorefugioIsChecked) {
+        markersFiltrados = markers.filter(marker =>
+            marker.options.veterinaria);
+    }
+    //VER Vete no, PELU SI, REFU SI
+    else if (!filtroVeterinariaIsChecked && filtropeluqueriaIsChecked
+        && filtrorefugioIsChecked) {
+
+        markersFiltrados = markers.filter(marker =>
+            marker.options.peluqueria || marker.options.refugio);
+    }
+    // VER vete NO, pelu SI, refu NO
+    else if (!filtroVeterinariaIsChecked && filtropeluqueriaIsChecked
+        && !filtrorefugioIsChecked) {
+
+        markersFiltrados = markers.filter(marker =>
+            marker.options.peluqueria);
+    }
+    //VETE NO, PELU NO, REFU SI
+    else if (!filtroVeterinariaIsChecked && !filtropeluqueriaIsChecked
+        && filtrorefugioIsChecked) {
+
+        markersFiltrados = markers.filter(marker =>
+            marker.options.refugio);
+    }
+}
+
+
+
+function crearMapa() {
+    map = L.map('map').setView([-34.52299128711134, -58.700488331227234], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
+
+function removerMarkers(arrDeMarkers){
+    map.eachLayer(function(layer) {
+  if (layer instanceof L.Marker) {
+    map.removeLayer(layer);
+  }
+});
+}
+
+function añadirMarkers(arrDeMarkers) {
+    console.log("añadiendo markers");
+    console.log(markersFiltrados);
+    arrDeMarkers.forEach((marker)=>{
+        marker.addTo(map);
     })
 }
 
-function exportarJson(){
-    return new Promise((resolve, reject) =>{
+function exportarJson() {
+    return new Promise((resolve, reject) => {
         let ret = [];
         let organizaciones = [];
         fetch("../Js/organizaciones.json")
-        .then(response => response.json())
-        .then(data => {
-            organizaciones = data;
-            organizaciones.forEach((org)=> {
-                var mensajePopup = "";
-                if (org.veterinaria){
-                    mensajePopup = mensajePopup + " Es veterinaria<br>"
-                }
-                if (org.peluqueria){
-                    mensajePopup = mensajePopup + " Es peluqueria<br>"
-                }
-                if (org.refugio){
-                    mensajePopup = mensajePopup + " Es un refugio <br>"
-                }
-                mark = new L.marker([org.direccion[0].coordenadas.x, org.direccion[0].coordenadas.y])
-                .addTo(map)
-                .bindPopup("Nombre: " + org.nombre + "<br>Direccion: " + org.direccion[0].direccion + ', ' 
-                + org.direccion[0].nombre_localidad + '<br>Actividades:<br>  ' + mensajePopup).openPopup()
-                ret.push(mark);
-            });
-            resolve(ret);
-        })
-        .catch(error => {
-            reject(error);
-        })
+            .then(response => response.json())
+            .then(data => {
+                organizaciones = data;
+                organizaciones.forEach((org) => {
+                    var mensajePopup = "";
+                    var veterinaria = false;
+                    var peluqueria = false;
+                    var refugio = false;
+                    if (org.veterinaria) {
+                        mensajePopup = mensajePopup + " Es veterinaria<br>"
+                        veterinaria = true;
+                    }
+                    if (org.peluqueria) {
+                        mensajePopup = mensajePopup + " Es peluqueria<br>"
+                        peluqueria = true;
+                    }
+                    if (org.refugio) {
+                        mensajePopup = mensajePopup + " Es un refugio <br>"
+                        refugio = true;
+                    }
+                    var mark = new L.marker([org.direccion[0].coordenadas.x, org.direccion[0].coordenadas.y], { veterinaria, refugio, peluqueria })
+                        .bindPopup("Nombre: " + org.nombre + "<br>Direccion: " + org.direccion[0].direccion + ', '
+                            + org.direccion[0].nombre_localidad + '<br>Actividades:<br>  ' + mensajePopup).openPopup()
+                    ret.push(mark);
+                });
+                resolve(ret);
+            })
+            .catch(error => {
+                reject(error);
+            })
     })
 }
