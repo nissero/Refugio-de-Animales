8def00e453a9f249a242376b97e38af8b4feb4f3
emiliano aguilar <1999kamerun@gmail.com>
2023-06-08 15:57:44 -0300

    mejora app.json

--- Js/app.js
+++ Js/app.js
@@ -1,14 +1,4 @@
 // creamos los obejtos de avisos
-class perfilAnimal {
-    constructor(nombre, raza, edad, descripcion, img) {
-        this.nombre = nombre;
-        this.raza = raza;
-        this.edad = edad;
-        this.descripcion = descripcion;
-        this.img = img;
-    }
-}
-
 
 class Aviso {
     constructor(titulo, perfilAnimal, telefono, tipoAviso, img) {
@@ -43,8 +33,7 @@ contenerDeProcutos.classList.add("segurosMostrados")
 let filtroPerdidoIsChecked = true;
 let filtroAdopcionIsChecked = true;
 let avisos = [];
-let avisosPerdidos = [];
-let avisosAdopcion = [];
+
 
 function exportarJson() {
     return new Promise((resolve, reject) => {
@@ -100,23 +89,13 @@ function checkPerdido() {
     });
 }
 
-function filtrarPerdido() {
-    avisosPerdidos = avisos.filter(Aviso => Aviso.tipoAviso == "perdidoEncontrado");
 
-}
-
-function filtrarAdopcion() {
-    avisosAdopcion = avisos.filter(Aviso => Aviso.tipoAviso == "Adopcion");
-}
 
 exportarJson().then(nuevoArrayAvisos => {
     avisos = nuevoArrayAvisos;
     console.log(avisos);
     console.log(avisosPerdidos);
     console.log(avisosAdopcion);
-    filtrarPerdido();
-    filtrarAdopcion();
-    mostrarAvisos();
 });
 
 function mostrarAvisos() {
@@ -124,18 +103,23 @@ function mostrarAvisos() {
     checkAdopcion();
     checkPerdido();
     console.log("entro")
+    // ver todos
     if (filtroAdopcionIsChecked && filtroPerdidoIsChecked) {
         console.log("los dos")
-        añadirAvisosAdopcionAlDoc();
-        añadirAvisosPerdidaAlDoc();
+      añadirAvisosAdopcionAlDoc(avisos);
     }
+
+    // ver solo adopcion
     else if (filtroAdopcionIsChecked && !filtroPerdidoIsChecked) {
-        console.log("adocpion")
-        añadirAvisosAdopcionAlDoc();
+        añadirAvisosAdopcionAlDoc(avisos.filter(Aviso => Aviso.tipoAviso == "Adopcion"));
+
+
     }
+    // ver solo perdido
     else if (!filtroAdopcionIsChecked && filtroPerdidoIsChecked){
-        console.log("perdido")
-        añadirAvisosPerdidaAlDoc();
+        añadirAvisosAdopcionAlDoc( avisosPerdidos = avisos.filter(Aviso => Aviso.tipoAviso == "perdidoEncontrado"));
+
+
     }
     else{
         console.log("no hizo nada")
@@ -145,17 +129,12 @@ function mostrarAvisos() {
 const botonActualizar = document.getElementById("actualizar");
 botonActualizar.addEventListener("click", mostrarAvisos);
 
-function añadirAvisosAdopcionAlDoc() {
-    avisosAdopcion.forEach((aviso) => {
+function añadirAvisosAdopcionAlDoc(avisosAMostrar) {
+    avisosAMostrar.forEach((aviso) => {
         const div = document.createElement(`div`)
         div.classList.add('aviso');
-        console.log(3);
         const image = document.createElement("img")
         image.src = aviso.img
-        //const image = document.createElement("img")
-        //  image.src = seguro.img
-        //image.classList.add("imagenSeguro")
-        console.log(323432432);
         div.innerHTML =
             `<div class="card" style="width: 18rem;">
             <img src="${aviso.img}" class="card-img-top" alt="...">
@@ -170,97 +149,8 @@ function añadirAvisosAdopcionAlDoc() {
             </ul>
           </div>`
         contenerDeProcutos.append(div)
-    })
-}
-
-
-
-
-function añadirAvisosPerdidaAlDoc() {
-    avisosPerdidos.forEach((aviso2) => {
-        const div = document.createElement(`div`)
-        div.classList.add('aviso');
-        console.log("imprimendo avisos de perdida");
-        const image = document.createElement("img")
-        image.src = aviso2.img
-        div.innerHTML =
-                `<div class="card" style="width: 18rem;">
-                <img src="${aviso2.img}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${aviso2.titulo}</h5>
-                  <p class="card-text">${aviso2.perfilAnimal}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">${aviso2.titulo}</li>
-                  <li class="list-group-item">${aviso2.cuidados}/li>
-                  <li class="list-group-item">${aviso2.telefono}</li>
-                </ul>
-              </div>`
-            contenerDeProcutos.append(div)
-    })
-}
-
-
-
-
-function añadirAvisoAlDoc() {
-    avisos.forEach((aviso) => {
-        const div = document.createElement(`div`)
-        console.log(3);
-        const image = document.createElement("img")
-        image.src = aviso.img
-        //const image = document.createElement("img")
-        //  image.src = seguro.img
-        //image.classList.add("imagenSeguro")
-        if (aviso.tipoAviso == "Adopcion") {
-            console.log(323432432);
-            div.innerHTML =
-                `<div class="card mb-3" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="${aviso.img}" class="img-fluid rounded-start" alt="..."> 
-                            </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title"> ${aviso.titulo}</h5>
-                                <p class="card-text">Tipo de aviso: ${aviso.tipoAviso}</p>
-                                <p class="card-text">Perfil del animal: ${aviso.perfilAnimal}</p>
-                                <p class="card-text"><small class="text-muted">  Cuidados: ${aviso.cuidados}  </small></p>
-                                <p class="card-text"><small class="text-muted">  Contacto: ${aviso.telefono}  </small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>`
-            contenerDeProcutos.append(div)
-        }
-        else if (aviso.tipoAviso == "perdidoEncontrado") {
-            div.innerHTML =
-                `<div class="card mb-3" style="max-width: 540px;">
-                                    <div class="row g-0">
-                                        <div class="col-md-4">
-                                            <img src="${aviso.img}" class="img-fluid rounded-start" alt="..."> 
-                                        </div>
-                                    <div class="col-md-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title"> ${aviso.titulo}</h5>
-                                            <p class="card-text">Tipo de aviso: ${aviso.tipoAviso}</p>
-                                            <p class="card-text">Perfil del animal: ${aviso.perfilAnimal}</p>
-                                            <p class="card-text"><small class="text-muted">  Fecha de encuentro: ${aviso.fecha}  </small></p>
-                                            <p class="card-text"><small class="text-muted">  Contacto: ${aviso.telefono}  </small></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`
-            contenerDeProcutos.append(div)
-        }
 
     })
 }
-
-
-
-
-
-
 
 
